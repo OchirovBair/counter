569d9dc7acde89aa8159d203410d40b04d497e1c
OchirovBair <pactuc@gmail.com>
2024-04-05 18:34:08 +0800

    reafactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {Counter} from "./Counter/Counter";
-import {BrowserRouter} from "react-router-dom";
 
 function App() {
     return (
--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from './styles/GlobalStyles';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App/>
         <GlobalStyles/>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
